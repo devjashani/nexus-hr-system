be6b1febbaab44f80eaaa677ee0abcb763a94f9a
Dev Jashani <devjashani2004@gmail.com>
2026-05-16 12:43:57 +0530

    Fixed JWT role based authentication and employee service security

---
+++ nexus-api-gateway/src/main/java/com/zidio/nexushr/gateway/NexusApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zidio.nexushr.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusApiGatewayApplication.class, args);
+    }
+
+}
